18fc099c95c87b36fc3357eecc2ce6d2971b908c
Bentley Hensel <bentley.hensel@civicactions.com>
2023-01-17 23:22:51 -0500

    oops
    this is not the api - maybe need to re-set it all up

---
+++ browser.js
@@ -0,0 +1,24 @@
+class Browser {
+  constructor(options) {
+    this.options = options;
+
+    this.window = null;
+    this.document = null;
+    this.statusCode = null;
+    this.contentType = null;
+    this.headers = null;
+    this.statusCode = null;
+    this.contentType = null;
+    this.html = null;
+    this.js = null;
+    this.links = null;
+    this.scripts = null;
+    this.cookies = null;
+  }
+}
+
+module.exports = Browser;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
